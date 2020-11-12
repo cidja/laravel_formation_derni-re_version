a032d26a31b0aac5c7e715ddcd22dcfd0d5b5fe4
cidja <chgeorgesc@gmail.com>
2020-11-12 14:00:56 +0100

    rajout de deux livres en plus dans les références

--- resources/views/ref.blade.php
+++ resources/views/ref.blade.php
@@ -16,5 +16,12 @@
         <li>écriture de blade <a href='https://laravel.sillo.org/laravel-4-chapitre-8-blade/'>https://laravel.sillo.org/laravel-4-chapitre-8-blade/</a></li>
         <li>Voir une première application laravel 8 : <a href='https://laravel.sillo.org/ma-premiere-application-laravel-8/#comment-3029'>https://laravel.sillo.org/ma-premiere-application-laravel-8/#comment-3029</a><li>
     </ol>
+
+    <h3>Des livres qui pourraient m'aider</h3>
+    <ol>
+        <li>coder proprement : <a href='https://www.amazon.fr/Coder-proprement-Robert-C-Martin/dp/232600227X/ref=as_li_ss_tl?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2KSEKHA4AJOXT&dchild=1&keywords=clean+code+francais&qid=1603457264&sprefix=clean+code+fran,aps,303&sr=8-1&linkCode=sl1&tag=jesuisundev-21&linkId=c1c0506218b0cb04b8fdb9dc7c7d20c8&language=fr_FR'>lien</a></li>
+        <li>Savoir ce concentrer: <a href='https://www.amazon.fr/Deep-work-retrouver-concentration-distractions/dp/B06XXVRZJC/ref=as_li_ss_tl?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=deep+work+francais&qid=1603550795&quartzVehicle=4015-1568&replacementKeywords=deep+francais&sr=8-1&linkCode=sl1&tag=jesuisundev-21&linkId=da2de53b97abaef986a728a184c3a59c&language=fr_FR'>lien</a></li>
+    </ol>
+    
 </body>
 </html>
