cfcbbae84db92c87d88868d19b6e94ebfec60ac2
cidja <chgeorgesc@gmail.com>
2020-11-16 21:50:28 +0100

    use asset to display images video link: https://www.youtube.com/watch?v=bwbiH3EB3T8&list=RDCMUCzuaB4F2znrMggxcwUuVhAw&index=15&ab_channel=LESTEACHERSDUNET

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'About-us | '. config('app.name'))
 
 @section('content')
-  <img src="/images/olympics.jpg" alt="logo olympics">
+  <img src="{{ asset('images/olympics.jpg') }}" alt="logo olympics">
   <p>Built with &hearts; by Chrichi</p>
 
   <p>
