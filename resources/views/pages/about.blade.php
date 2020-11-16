c4581cf2b2ad2c9647208fb6ae1871a78400cb70
cidja <chgeorgesc@gmail.com>
2020-11-16 21:35:43 +0100

    add images on public/images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,10 +1,12 @@
 @extends('app')
 
-@section('title', 'About-us | '. config('app.name')) 
- 
-@section('content')
+@section('title', 'About-us | '. config('app.name'))
 
-        <p>Built with &hearts; by Chrichi</p>
-         
-         <p><a href="{{ route('home') }}">Retourner à la page d'accueuil</a> 
+@section('content')
+  <img src="/images/olympics.jpg" alt="logo olympics">
+  <p>Built with &hearts; by Chrichi</p>
+
+  <p>
+    <a href="{{ route('home') }}">Retourner à la page d'accueuil</a>
+  </p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,7 @@
 @section('title', config('app.name'))
 
 @section('content')
+    <img src="/images/nrj.png" alt="logo NRJ">
           <h1>Hello from France</h1>
 
           <p>It's currently {{ date('d-M-Y')}} and time is {{ date(DATE_RSS) }} </p>
