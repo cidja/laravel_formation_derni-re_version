6c21df38e30e3ea34a8c4ba8d1a6d3ded6819ea6
cidja <chgeorgesc@gmail.com>
2020-11-16 21:57:43 +0100

    update app.blade with env lang https://www.youtube.com/watch?v=XQ9c36b0G5Q&list=RDCMUCzuaB4F2znrMggxcwUuVhAw&index=26&ab_channel=LESTEACHERSDUNET

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"> <!--selectionne la langue qui est enregistré dans la variable env dans app.php !-->
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,12 +8,12 @@
             @yield('title', config('app.name'))
         </title>
 
-        
+
     </head>
     <body>
         @yield('content')
         <!--Pour accéder à une variable de configuration qui se trouve dans app !-->
-        {{ config('app.name')}} 
+        {{ config('app.name')}}
         <!--Pour vérifier ce que retourne la fonction on utilise dump !-->
         {{ dump(config('database.connections.sqlite.dirver'))}}
 
@@ -23,7 +23,7 @@
             @if(! Route::is('about')) <!--affiche le lien about uniquement si on est sur une page différente de about !-->
 
             &middot; <a href="{{ route('about') }}">About Us</a></p>
-            
+
             @endif
         </footer>
     </body>
