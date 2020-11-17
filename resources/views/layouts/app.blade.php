c355bddcdabdf388cff2b05d2a4ab1c7c6f20b0d
cidja <chgeorgesc@gmail.com>
2020-11-17 13:15:17 +0100

    add layouts folder and _footer.blade.php for footer video link https://www.youtube.com/watch?v=LYVpQXlSmCg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,15 +21,7 @@
         {{ config('app.name')}}
         <!--Pour vérifier ce que retourne la fonction on utilise dump !-->
         {{ dump(config('database.connections.sqlite.dirver'))}}
+        @include('layouts/partials/_footer')
 
-        <footer>
-           <p class="text-gray-400">
-           &copy; Copyright {{date('Y')}}
-            @if(! Route::is('about')) <!--affiche le lien about uniquement si on est sur une page différente de about !-->
-
-            &middot; <a href="{{ route('about') }}" class="text-indigo-500 hover:text-indigo-600 underline" >About Us</a></p>
-
-            @endif
-        </footer>
     </body>
 </html>
